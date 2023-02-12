e61502d64be32fd1a4ee5fca814cbaf7817bb921
SQ QAISAR <isqqaisar@gmail.com>
2023-02-12 22:36:50 +0500

    Building Recycler View for displaying aya

---
+++ app/src/main/java/com/example/qurannavigator/displayingAyah.java
@@ -0,0 +1,14 @@
+package com.example.qurannavigator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class displayingAyah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaying_ayah);
+    }
+}
