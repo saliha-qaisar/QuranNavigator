06d630b18296e292c11b8b91ab11e0b59694fb35
SQ QAISAR <isqqaisar@gmail.com>
2023-02-12 22:16:42 +0500

    getting surahs name

--- app/src/main/java/com/example/qurannavigator/by_Parah.java
+++ app/src/main/java/com/example/qurannavigator/by_Parah.java
@@ -6,8 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class by_Parah extends AppCompatActivity {
     RecyclerView recycle ;
@@ -15,6 +23,7 @@ public class by_Parah extends AppCompatActivity {
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
     String selectedFormt;
+    ArrayList<String> surahs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,7 @@ public class by_Parah extends AppCompatActivity {
         selectedFormt = intent.getStringExtra("format");
         recycle=(RecyclerView)findViewById(R.id.recyclerView);
         dataHolder=new ArrayList<>();
+        surahs=new ArrayList<>() ;
         layoutManager = new LinearLayoutManager(by_Parah.this);
         recycle.setLayoutManager(layoutManager);
         if(selectedFormt.equals("parah"))
@@ -39,10 +49,52 @@ public class by_Parah extends AppCompatActivity {
         }
         else
         {
+            try{
+                JSONObject obj=new JSONObject(loadJson());
+                JSONArray array=obj.getJSONArray("data");
+                for (int i=0;i<array.length();i++)
+                {
+                    JSONObject sbj=array.getJSONObject(i);
+                    if(!surahs.contains(sbj.getString("englishName")))
+                    {
+                        surahs.add(sbj.getString("englishName"));
+
+                    }
+
+
+                }
+
+                Log.d("array","lenth"+surahs.size());
+
+            }
+            catch (JSONException e)
+            {
+
+            }
 
         }
         adapter = new myRecyclerViewAdapter(dataHolder) ;
         recycle.setAdapter(adapter);
 
     }
+     public String loadJson(){
+        String json=null;
+        try{
+            InputStream in=getAssets().open("QuranMetaData.json");
+            int size=in.available();
+            byte[] buffer=new byte[size];
+            in.read(buffer);
+            in.close();
+            json=new String(buffer,"UTF-8");
+            Log.d("array","Done");
+
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        return json;
+     }
 }
